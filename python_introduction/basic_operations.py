6fff40a9eb7af650cac4f27273e7f38694c89e83
Seth Kipsang Mutuba <61922630+sethkipsangmutuba@users.noreply.github.com>
2025-09-21 21:32:08 +0300

    Create basic_operations.py
    Add basic_operations.py with addition, subtraction, and multiplication

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+# Assign the numbers
+number1 = 10
+number2 = 5
+
+# Perform addition
+addition = number1 + number2
+print(f"Addition of {number1} and {number2} is {addition}")
+
+# Perform subtraction
+subtraction = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+
+# Perform multiplication
+multiplication = number1 * number2
+print(f"Multiplication of {number1} and {number2} is {multiplication}")
